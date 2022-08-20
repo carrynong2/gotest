f8cdb6f1bd4f343726baf6685a7421483210cffb
Monthon Nong <carrynong2@gmail.com>
2022-08-20 16:57:53 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module gotest
+
+go 1.13
+
+require (
+	github.com/gofiber/fiber/v2 v2.36.0
+	github.com/stretchr/testify v1.8.0
+)
---
+++ services/promotion_test.go
@@ -0,0 +1,83 @@
+package services_test
+
+import (
+	"errors"
+	"gotest/repositories"
+	"gotest/services"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPromotionCalculateDiscount(t *testing.T) {
+
+	type testCase struct {
+		name            string
+		purchaseMin     int
+		discountPercent int
+		amount          int
+		exptected       int
+	}
+
+	cases := []testCase{
+		{name: "applied 100", purchaseMin: 100, discountPercent: 20, amount: 100, exptected: 80},
+		{name: "applied 200", purchaseMin: 100, discountPercent: 20, amount: 200, exptected: 160},
+		{name: "applied 300", purchaseMin: 100, discountPercent: 20, amount: 300, exptected: 240},
+		{name: "not applied 50", purchaseMin: 100, discountPercent: 20, amount: 50, exptected: 50},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			// Arrange
+			promoRepo := repositories.NewPromotionRepositoryMock()
+			promoRepo.On("GetPromotion").Return(repositories.Promotion{
+				ID:              1,
+				PurchaseMin:     c.purchaseMin,
+				DiscountPercent: c.discountPercent,
+			}, nil)
+
+			promoService := services.NewPromotionService(promoRepo)
+
+			// Act
+			discount, _ := promoService.CalculateDiscount(c.amount)
+			expected := c.exptected
+
+			// Assert
+			assert.Equal(t, expected, discount)
+		})
+	}
+
+	t.Run("purchase amount zero", func(t *testing.T) {
+		// Arrange
+		promoRepo := repositories.NewPromotionRepositoryMock()
+		promoRepo.On("GetPromotion").Return(repositories.Promotion{
+			ID:              1,
+			PurchaseMin:     100,
+			DiscountPercent: 20,
+		}, nil)
+
+		promoService := services.NewPromotionService(promoRepo)
+
+		// Act
+		_, err := promoService.CalculateDiscount(0)
+
+		// Assert
+		assert.ErrorIs(t, err, services.ErrZeroAmount)
+		promoRepo.AssertNotCalled(t, "GetPromotion")
+	})
+
+	t.Run("repository error", func(t *testing.T) {
+		// Arrange
+		promoRepo := repositories.NewPromotionRepositoryMock()
+		promoRepo.On("GetPromotion").Return(repositories.Promotion{}, errors.New(""))
+
+		promoService := services.NewPromotionService(promoRepo)
+
+		// Act
+		_, err := promoService.CalculateDiscount(100)
+
+		// Assert
+		assert.ErrorIs(t, err, services.ErrRepository)
+	})
+
+}
